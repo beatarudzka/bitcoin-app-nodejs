93d8e0c51ae7520c4e987dcdbc0d28455dd20907
Beata Rudzka <beatarudzka89@gmail.com>
2019-11-01 04:48:52 +0100

    Using the Request Module to Get Data from an API

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', function (err, res, body) {
+  console.log(body)
+})
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function (req, res) {
+  console.log(req.body.crypto)
 })
 
 app.listen(4000, function () {
