9f377be16fd0a7e377a8b9fdfbda1b5edbf5e595
Beata Rudzka <beatarudzka89@gmail.com>
2019-11-01 03:53:14 +0100

    creating server and simple drop down list

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(4000, function () {
+  console.log("Server is running on port 4000")
+})
+
